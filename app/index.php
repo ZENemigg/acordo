88aea9dfe4a1b84a5bff8b24d1264141cdddf264
ZENemigg <luiza.gimenez@outlook.com>
2023-08-24 17:49:23 -0400

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
+<?php
+    echo "<h1> teste </h1>";
+    echo "<input type= text>";
+    echo  "<button type = submit> send </button>";
+    echo date('h:i:s A');
+?>
+</body>
+</html>
+
+
+
