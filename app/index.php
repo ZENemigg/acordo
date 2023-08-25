ff1b1688f8d2cfc3d9bf2fac89b9e4fb605fa372
ZENemigg <luiza.gimenez@outlook.com>
2023-08-25 17:34:43 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -6,11 +6,25 @@
     <title>Teste</title>
 </head>
 <body>
+    <form action="calculo.php" method="POST" class="form-group col-6 mx-auto">
+        <div class="form-group">
+            <label for="">Digite seu peso</label>
+            <input type="number" class="form-control" name="peso">
+        </div>
+        <div class="form-group">
+            <label for="">Digite sua altura</label>
+            <input type="text" class="form-control" name="altura">
+            <button class="btn btn-secondary btn-block">Calcular</button>
+        </div>
+    </form>
 <?php
-    echo "<h1> teste </h1>";
-    echo "<input type= text>";
-    echo  "<button type = submit> send </button>";
-    echo date('h:i:s A');
+    $peso = $_POST['peso'];
+    $altura = $_POST['altura'];
+    $imc = $peso/pow($altura,2);
+    echo "<h1 class='text-center>Resultado</h1>";
+    if ($imc < 18.5) {
+        echo "<input type='text' class= 'form-control col-6 mx-auto' value=" .$imc. ">";
+    }
 ?>
 </body>
 </html>
